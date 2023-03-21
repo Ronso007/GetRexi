f647a7a50a4665e1c16f46a153e551d87819679d
Ron Soffer <ronso008@gmail.com>
2023-03-22 01:36:07 +0200

    bug fixes

--- app/src/main/java/com/ronsapir/getRexi/auth/data/AddOrEditDogFragment.java
+++ app/src/main/java/com/ronsapir/getRexi/auth/data/AddOrEditDogFragment.java
@@ -24,13 +24,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
 import com.ronsapir.getRexi.R;
@@ -42,12 +46,17 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -134,14 +143,56 @@ public class AddOrEditDogFragment extends Fragment {
         setGalleryButtonActionListener();
 
         Spinner dogBreed = view.findViewById(R.id.dogBreed);
-        DogApi.instance().getBreedOptions(optionsList -> {
+/*        DogApi.instance().getBreedOptions(optionsList -> {
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, optionsList);
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             dogBreed.setAdapter(adapter);
-        });
+        });*/
+        AdapterView.OnItemSelectedListener listener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+        dogBreed.setOnItemSelectedListener(listener);
+        getOptionsFromApi(dogBreed);
         return view;
     }
+
+    private void getOptionsFromApi(Spinner dogBreed ) {
+        String url = "https://api.thedogapi.com/v1/breeds";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+                    List<String> optionsList = new ArrayList<>();
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            String option = jsonObject.getString("name");
+                            optionsList.add(option);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    // Populate the options in the dropdown
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, optionsList);
+
+
+
+
+                    adapter.setDropDownViewResource(R.layout.spinner_style);
+                    dogBreed.setAdapter(adapter);
+                },
+                error ->  Log.e("error", error.getMessage()));
+
+        // Add the request to the Volley request queue
+        Volley.newRequestQueue(getContext()).add(jsonArrayRequest);
+    }
+
 
     private void setFields() {
         Bundle bundle = getArguments();
@@ -169,33 +220,71 @@ public class AddOrEditDogFragment extends Fragment {
 
             Dog dogToSave = new Dog(dogName, dogBreed, dogAge, imageUrl, "", userId, "");
 
-            DogApi.instance().getDogTemperament(dogBreed, dogToSave::setTemperament);
-
-            DogApi.instance().getLifeSpan(dogBreed, dogToSave::setLifeSpan);
-
-            Bundle bundle = getArguments();
-            if (bundle.get("Dog") != null) {
-                Dog dog = (Dog) bundle.get("Dog");
-                dogToSave.setImageUrl(dog.getImageUrl());
-            }
-
-            if (isImageSelected) {
-                binding.dogImg.setDrawingCacheEnabled(true);
-                binding.dogImg.buildDrawingCache();
-                Bitmap bitmap = ((BitmapDrawable) binding.dogImg.getDrawable()).getBitmap();
-                Model.instance().uploadImage(dogName, bitmap, url->{
-                    if (url != null){
-                        dogToSave.setImageUrl(url);
+            getDogTemperament(dogBreed, temperemant -> {
+                getLifeSpan(dogBreed, lifespan -> {
+                    dogToSave.setTemperament(temperemant);
+                    dogToSave.setLifeSpan(lifespan);
+                    Bundle bundle = getArguments();
+                    if (bundle.get("Dog") != null) {
+                        Dog dog = (Dog) bundle.get("Dog");
+                        dogToSave.setImageUrl(dog.getImageUrl());
                     }
-                    Model.instance().addDog(dogToSave, (unused) -> {
-                        Navigation.findNavController(view1).popBackStack();
-                    });
+
+                    if (isImageSelected) {
+                        binding.dogImg.setDrawingCacheEnabled(true);
+                        binding.dogImg.buildDrawingCache();
+                        Bitmap bitmap = ((BitmapDrawable) binding.dogImg.getDrawable()).getBitmap();
+                        Model.instance().uploadImage(dogName, bitmap, url -> {
+                            if (url != null) {
+                                dogToSave.setImageUrl(url);
+                            }
+                            Model.instance().addDog(dogToSave, (unused) -> {
+                                Navigation.findNavController(view1).popBackStack();
+                            });
+                        });
+                    } else {
+                        Model.instance().addDog(dogToSave, (unused) -> {
+                            Navigation.findNavController(view1).popBackStack();
+                        });
+                    }
                 });
-            } else {
-                Model.instance().addDog(dogToSave, (unused) -> {
-                    Navigation.findNavController(view1).popBackStack();
-                });
-            }
+            });
         });
+    }
+
+    public void getDogTemperament(String dogBreed, Model.Listener<String> callback) {
+        String url = "https://api.thedogapi.com/v1/breeds/search?name=" + dogBreed;
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+                        try {
+                            JSONObject json = response.getJSONObject(0);
+                            String temperament = json.getString("temperament");
+                            callback.onComplete(temperament);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                },
+                error ->  Log.e("error", error.getMessage()));
+
+        // Add the request to the Volley request queue
+        Volley.newRequestQueue(getContext()).add(jsonArrayRequest);
+    }
+
+    public void getLifeSpan(String dogBreed, Model.Listener<String> callback) {
+        String url = "https://api.thedogapi.com/v1/breeds/search?name=" + dogBreed;
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        JSONObject json = response.getJSONObject(0);
+                        String lifespan = json.getString("life_span");
+                        callback.onComplete(lifespan);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error ->  Log.e("error", error.getMessage()));
+
+        // Add the request to the Volley request queue
+        Volley.newRequestQueue(getContext()).add(jsonArrayRequest);
     }
 }
